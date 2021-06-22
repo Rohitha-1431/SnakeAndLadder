2a1b99aebd63cbed4ea06becb1fcfa6a13712aa9
unknown <rohithasatya314@gmail.com>
2021-06-22 23:34:58 +0530

    MODIFIED:playr rolls the die

---
+++ SnakeAndLadderUC2.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderUC2 {
+	public static void main(String[] args) {
+		int die=(int)(Math.random()*6) + 1;
+		System.out.println("player rolls up die:" +die);
+	}
+}

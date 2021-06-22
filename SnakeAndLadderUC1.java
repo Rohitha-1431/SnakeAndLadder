ce4eb7ab356d1baf4fdea661970a683814645ab5
unknown <rohithasatya314@gmail.com>
2021-06-22 23:25:58 +0530

    MODIFIED:starting position of player

---
+++ SnakeAndLadderUC1.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class SnakeAndLadderUC1 {
+	public static void main(String[] args) {
+		int player1 = 0;
+		System.out.println("Start position is:" +player1);
+	}
+
+}
